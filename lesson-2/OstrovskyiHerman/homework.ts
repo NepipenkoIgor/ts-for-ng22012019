101491d29ec69e5ca7ddaddac78bb49bb255e639
Ostrovskyi <german.ostrovskij@gmail.com>
2019-01-30 00:32:14 +0100

    function summator  was created

--- lesson-2/OstrovskyiHerman/homework.ts
+++ lesson-2/OstrovskyiHerman/homework.ts
@@ -1,5 +1,3 @@
-
-
 //   1) Написать функцию isInArray(), которая начиная со второго принимает переменное количество аргументов.
 //   Возвращает true, если все аргументы, кроме первого входят в первый.
 //   Первым всегда должен быть массив.
@@ -14,3 +12,18 @@ function isInArray(checkArr: number[], ...args: number[]): boolean {
 
     return inArray;
 }
+
+
+// 2) Написать функцию summator(), которая суммирует переданые ей аргументы.
+// Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
+type stringOrNumber = string | number;
+
+
+function summator(...args: stringOrNumber[]): stringOrNumber {
+    if (typeof args[0] === 'string') {
+        return args.join('');
+    } else {
+        return args.reduce((prev: stringOrNumber, currentValue: stringOrNumber) => Number(prev) + Number(currentValue));
+    }
+}
+
