79e1ec09d9c6e57050b0928ea66ec3848974d52b
Yuri Stepanov <stepanoff.yura@ya.ru>
2019-02-01 01:50:41 +0300

    bump build

--- lesson-2/StepanovYuri/homework.ts
+++ lesson-2/StepanovYuri/homework.ts
@@ -20,7 +20,6 @@ type stringOrNumber = string | number;
 
 function summator(...rest: stringOrNumber[]): number {
   return rest.reduce((prev: number, current: stringOrNumber) => {
-    // console.log('prev ' + prev + ' current ' + current);
     if (typeof current === 'string') {
       return parseInt(current) + prev;
     } else {
@@ -34,15 +33,11 @@ function summator(...rest: stringOrNumber[]): number {
 // 3
 function getUnique(...arr: stringOrNumber[]): stringOrNumber[] {
   return arr.filter((value: stringOrNumber, index: number, array: stringOrNumber[]) => {
-    // tslint:disable-next-line:no-console
-    // console.log('v ', value, 'i ', index, 'a ', array, 'exp', array.indexOf(value) === index);
-
     return array.indexOf(value) === index;
   });
   // return [...new Set(arr)];
 }
 
-// tslint:disable-next-line:no-console
 // console.log(getUnique(1, 2, 3, 1, '2', '3'));
 
 // 4
@@ -55,5 +50,4 @@ function toMatrix(data: stringOrNumber[], rowSize: number): stringOrNumber[][] {
   return result;
 }
 
-// tslint:disable-next-line:no-console
-toMatrix([1, 2, 3, 4, 5, 6, 7, 8, 9], 2);
+// console.log(toMatrix([1, 2, 3, 4, 5, 6, 7, 8, 9], 2));
