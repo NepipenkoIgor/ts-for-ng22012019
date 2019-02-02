9cc22e852121e7b9c46ce84e55bf6f6d4f11d78e
Evgeniy Barinov <z.barinov@gmail.com>
2019-02-02 15:31:03 +0300

    lesson - 2: tested

--- lesson-2/BarinovEvgeniy/homework.js
+++ lesson-2/BarinovEvgeniy/homework.js
@@ -1,38 +1,60 @@
+"use strict";
 /**
  1)
  Написать функцию isInArray(), которая начиная со второго принимает переменное количество аргументов.
  Возвращает true, если все аргументы, кроме первого входят в первый.
  Первым всегда должен быть массив.
  **/
-
-type stringOrNumber = string | number;
-
-export function isInArray(arr: stringOrNumber[], ...targets: stringOrNumber[]): boolean {
-    return targets.every((item: stringOrNumber) => arr.includes(item));
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spread = (this && this.__spread) || function () {
+    for (var ar = [], i = 0; i < arguments.length; i++) ar = ar.concat(__read(arguments[i]));
+    return ar;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+function isInArray(arr) {
+    var targets = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        targets[_i - 1] = arguments[_i];
+    }
+    return targets.every(function (item) { return arr.includes(item); });
 }
-
+exports.isInArray = isInArray;
 /**
  2)
  Написать функцию summator(), которая суммирует переданые ей аргументы.
  Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
  **/
-
-export function summator(...args: stringOrNumber[]): number {
-    return args.reduce(
-        (res: number, value: stringOrNumber): number => {
-            value = typeof (value) === 'string'
-                ? parseFloat(value)
-                : value;
-
-            if (isNaN(value)) {
-                value = 0;
-            }
-
-            return res + value;
-        }, 0
-    );
+function summator() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return args.reduce(function (res, value) {
+        value = typeof (value) === 'string'
+            ? parseFloat(value)
+            : value;
+        if (isNaN(value)) {
+            value = 0;
+        }
+        return res + value;
+    }, 0);
 }
-
+exports.summator = summator;
 /**
  * 3)
  Написать функцию getUnique(arr), которая принимает аргументом неограниченое число аргументов,
@@ -40,15 +62,17 @@ export function summator(...args: stringOrNumber[]): number {
  Порядок элементов результирующего массива должен совпадать с порядком,
  в котором они встречаются в оригинальной структуре.
  */
-
-export function getUnique(...args: stringOrNumber[]): stringOrNumber[] {
-    return [...new Set([...args])];
+function getUnique() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return __spread(new Set(__spread(args)));
     // return args.filter((item: stringOrNumber, i: number, _args: stringOrNumber[]) => {
     //     return i === _args.indexOf(item);
     // });
 }
-
-
+exports.getUnique = getUnique;
 /**
  * 4)
  Дописать функцию toMatrix(data, rowSize), которая принимает аргументом массив и число,
@@ -56,26 +80,20 @@ export function getUnique(...args: stringOrNumber[]): stringOrNumber[] {
  элементы подмассивов беруться из массива data.
  Оригинальный массив не должен быть изменен.
  */
-
-export function toMatrix(data: stringOrNumber[], rowSize: number): stringOrNumber[][] {
-    const matrix: stringOrNumber[][] = [];
-
-    data.forEach((_value: stringOrNumber, index: number) => {
+function toMatrix(data, rowSize) {
+    var matrix = [];
+    data.forEach(function (_value, index) {
         if (index % rowSize !== 0) {
             return;
         }
         matrix.push(data.slice(index, index + rowSize));
     });
-
     return matrix;
 }
-
-
+exports.toMatrix = toMatrix;
 /**
  * 5)* https://learn.javascript.ru/task/debounce
  */
-
-
 // export class MathLib {
 //
 //     @debounce(1000)
@@ -85,7 +103,6 @@ export function toMatrix(data: stringOrNumber[], rowSize: number): stringOrNumbe
 //     }
 //
 // }
-
 // function debounce(ms: number): Function {
 //     return (
 //         _target: Object,
@@ -113,12 +130,9 @@ export function toMatrix(data: stringOrNumber[], rowSize: number): stringOrNumbe
 //         };
 //     };
 // }
-
-
 /**
  * 6)* https://learn.javascript.ru/task/throttle
  */
-
 // function throttle(ms: number): Function {
 //     return (
 //         _target: Object,
@@ -155,8 +169,6 @@ export function toMatrix(data: stringOrNumber[], rowSize: number): stringOrNumbe
 //
 //     };
 // }
-
-
 /**
  * checking
  */
@@ -170,3 +182,4 @@ export function toMatrix(data: stringOrNumber[], rowSize: number): stringOrNumbe
 // }, 100);
 //
 // setTimeout(() => clearInterval(interval), 10000);
+//# sourceMappingURL=homework.js.map
