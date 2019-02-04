7ac1f4ec1be2483ce1a4d49a08e9ff01cfe86059
Ostrovskyi <german.ostrovskij@gmail.com>
2019-02-04 18:20:59 +0100

    Home work 2 was tested

---
+++ karma.conf.js
@@ -0,0 +1,22 @@
+module.exports = (config) => {
+    config.set({
+        frameworks: ['jasmine', 'karma-typescript'],
+        files: [
+            {
+                pattern: './lesson-2/OstrovskyiHerman/**/*.ts'
+            }],
+        preprocessors: {
+            "**/*.ts": ["karma-typescript"]
+        },
+        reporters: ['progress', 'karma-typescript'],
+        browsers: ["ChromeHeadless"],
+        singleRun: false,
+        karmaTypescriptConfig: {
+            tsconfig: './lesson-2/OstrovskyiHerman/tsconfig.json',
+            reports: {
+                "html": "coverage",
+                "text": ""
+            }
+        }
+    })
+};
