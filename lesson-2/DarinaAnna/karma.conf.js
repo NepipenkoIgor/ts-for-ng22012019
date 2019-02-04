f1448e7059c0b639e1f361041d9ebed1012846e5
Anna Darina <anndarina@gmail.com>
2019-02-04 21:21:16 +0300

    some refactor and functions tests

---
+++ lesson-2/DarinaAnna/karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = (config) => {
+    config.set({
+        frameworks: ["jasmine", "karma-typescript"],
+        files: [
+            { pattern: './lesson-2/DarinaAnna/**/*.ts' }
+        ],
+        preprocessors: {
+            "**/*.ts": ["karma-typescript"]
+        },
+        reporters: ["progress", "karma-typescript"],
+        browsers: ["ChromeHeadless"],
+        singleRun: true,
+        karmaTypescriptConfig: {
+            tsconfig: "./lesson-2/DarinaAnna/tsconfig.json",
+            reports: {
+                "html": "coverage",
+                "text": ""
+            }
+        }
+    })
+}
