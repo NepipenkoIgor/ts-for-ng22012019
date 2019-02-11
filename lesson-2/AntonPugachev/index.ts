763be6a174c7c3e2018a5d7dc2ae53b3eb6fd4e1
16731075 <pugachev.a.a@sberbank.ru>
2019-02-11 11:25:11 +0300

    'fix:hw'

--- lesson-2/AntonPugachev/index.ts
+++ lesson-2/AntonPugachev/index.ts
@@ -1,36 +1,93 @@
-import { Subject } from 'rxjs';
+import { fromEvent, Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 type tItem = { id: number, name: string };
-
+// ответ от сервера
 const stream$$: Subject<tItem[]> = new Subject;
+// получение данных
+stream$$.subscribe((data: tItem[]) => {
+    console.log(data);
+    $('.list li').remove();
+    data.forEach((item: tItem) => {
+        $('.list').append(`<li class='item'>${item.name}</li>`);
+        console.log('test');
+
+    });
+});
 
 
 /** чисто получение репо*/
-function getDataFromGit(): void {
+function getData(search: string): void {
     $.ajax({
         url: 'https://api.github.com/users/mun4kin/repos',
         method: 'GET',
         dataType: 'json',
         // tslint:disable-next-line
         success: function (res: tItem[]) {
-            stream$$.next(res.map((item: tItem) => {
-                return {
-                    id: item.id,
-                    name: item.name
-                };
-            }));
+            stream$$.next(res
+                .filter((item: tItem) => {
+                   return  ~item.name.toLowerCase().indexOf(search.toLowerCase());
+                })
+                .map((item: tItem) => {
+                    return {
+                        id: item.id,
+                        name: item.name
+                    };
+                }));
         }
     });
 }
 
-stream$$.subscribe((data: tItem[]) => {
-    console.log(data);
-    data.forEach((item: tItem) => {
-        $('.list').append(`<li class='item'>${item.name}</li>`);
-    });
+//  класс для поиска
+export class Homework {
+    @debounce(500)
+    public getDataFromGit(search: string): string {
+        getData(search);
+        return search;
+    }
+}
 
+const myClass: Homework = new Homework;
+// вытаскиваем значение для поиска
+const source$: Observable<String> = fromEvent($('.live-search'), 'keyup')
+    .pipe(
+        map((ev: Event) => {
+            return (ev.target as HTMLInputElement).value;
+        })
+    );
+
+source$.subscribe((val: string) => {
+    myClass.getDataFromGit(val);
 });
 
 
-getDataFromGit();
+// отложенный запуск поиска
+function debounce(ms: number): Function {
+    return (
+        _target: Object,
+        _methodName: string,
+        descriptor: PropertyDescriptor
+    ): PropertyDescriptor => {
+        const originalDescriptorValue: Function = descriptor.value;
+        let timer: number | null = null;
+
+        // tslint:disable-next-line
+        function decorated(...args: any[]): void {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => {
+                timer = null;
+                originalDescriptorValue.apply(_target, args);
+            }, ms);
+        }
+
+        return {
+            ...descriptor,
+            value: decorated
+        };
+    };
+}
+
 
