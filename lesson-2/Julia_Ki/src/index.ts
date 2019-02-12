5a48ed4a63b9897c4fe21b173f248627da2b43c8
Julia <ip018@hdm-stuttgart.de>
2019-02-12 08:29:40 +0100

    Переработка материала RxJS - 2

--- lesson-2/Julia_Ki/src/index.ts
+++ lesson-2/Julia_Ki/src/index.ts
@@ -1,302 +1,337 @@
-import { interval, Observable, of, zip } from 'rxjs';
-import {  map, take } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { map, observeOn } from 'rxjs/operators';
+import { asap } from 'rxjs/internal/scheduler/asap';
 
-// import { Observable, Subscriber } from 'rxjs';
-
-// from создает стандартный объект Observable и перебирает переданные эл-ты
-// from - конечный
-
-// const sequence$: Observable<number> = from ([1,2,3,4]);
-
-// --------------------------
-
-// создаем наш объект типа Observable и задаем ему конструктор. Здесь создается поток
-// let count: number = 0;
-// const sequence1$: Observable<number> = Observable.create((subscriber: Subscriber<number>) => {
-//
-//     setInterval(() => {
-//         count++;
-//         subscriber.next(count);
-//     }, 1000);
-//
-// });
-//
-// sequence1$.subscribe((value: number) => {
-//         console.log('subscribe 1 ----> ', value);
-//     }, () => {},
-//     () => {
-//         console.log('complete');
-//     });
-//
-// // второй подписчик получает данные с самого начала (если только count находится внутри нашего Observable)
-// setTimeout( () => {
-//     sequence1$.subscribe((value: number) => {
-//             console.log('subscribe 2 ----> ', value);
-//         }, () => {},
-//         () => {
-//             console.log('complete');
-//         });
-// }, 4000);
-
-
-// -----------------
-// of - конечный
-
-// const sequence2$: Observable<number[] | number> = of([1,2,3,4], 1);
-//
-// sequence2$.subscribe((value: number[] | number) => {
-//     console.log(value);
-// }, () => {},
-//     () => {
-//     console.log('complete');
-//     });
-//
-
-// ---------------
-// fromEvent - бесконечный
-
-// const el: HTMLInputElement = document.querySelector('.live-search') as HTMLInputElement;
-// const sequence3$: Observable<Event> = fromEvent(el, 'input');
-
-// sequence3$.subscribe((event: Event) => {
-//     console.log('subscribe 1 ----> ', (event.target as HTMLInputElement).value);
-// }, () => {},
-//     () => {
-//         console.log('complete'); // не сработает, тк поток бесконечный
-//     });
-// через 4 сек после запуска первого потока
-// при этом второй поток ничего не знает о первом, они не зависимы (ГОРЯЧИЙ поток)
-// setTimeout( () => {
-//     sequence3$.subscribe((event: Event) => {
-//             console.log('subscribe 2 ----> ', (event.target as HTMLInputElement).value);
-//         }, () => {},
-//         () => {
-//             console.log('complete');
-//         });
-// }, 4000);
-
-
-// -----------------------
-// RxJS == Lodash for Stream
-// from - конечный
-
-// const sequence4$: Observable<number> = from([1, 2, 3, 4]);
-//
-// const sequence5$: Observable<number> = sequence4$.pipe(
-//     map((val: number) => val ** 3),
-//     filter((val: number) => val % 3 === 0)
-//     // operators
-// );
-
-/*
-* sequence4$ --1--2--3--4 |
-*       map((val: number) => val ** 3)
-*           --1--8--27--64 |
-*       filter((val: number) => val % 3 === 0)
-* sequence4$ --------27---- |
-*
-* */
-
-// sequence5$.subscribe((value: number) => {
-//     console.log('subscribe 1 ----> ', value);
-// }, () => {},
-//     () => {
-//     console.log('complete');
-//     });
-
-
-// ----------
-/* TAP ничего не  делает */
-// interval - бесконечный
-
-// const sequence6$: Observable<number> = interval(1000);
-
-/*
-* sequence6$ --0--1--2--3--4-- ... и т.д.
-* */
-// const sequence7$: Observable<number> = sequence6$
+// обработка ошибок
+// const sequense$: Observable<number | string> = interval(1000)
 //     .pipe(
-//         tap((x: number) => {
-//             console.log('x ---> ', x);
-//             return x ** 3 + 2;
+//         map((value: number) => {
+//             if (value % 3 === 0 && value !== 0) {
+//                 return value.toString();
+//             }
+//             return value;
 //         })
 //     );
 //
-// sequence7$.subscribe((value: number) => {
-//             console.log('subscribe 1 ----> ', value);
-//         }, () => {},
-//         () => {
-//             console.log('complete');
+// sequense$
+//     .pipe(
+//
+//         // (*) создать поток высшего порядка: switchMap(), mergeMap()
+//         // switchMap это как переключение на другой поток, в данном случа of. switchMap делает subscribe в subscribe
+//         // 1--2--3--4--5--6--
+//         // |  |  |  |  |  |
+//         // 1--2--3--4--5--6--   <-- каждое значение это конечный поток с текущим значением value
+//
+//         // НАШ ПОТОК НЕ БУДЕТ ПРЕРВАН
+//         switchMap((value: number) => {
+//             //  возвращается новый поток с одного примитива value
+//             return of(value)
+//                 .pipe(
+//                     map((v: number) => {
+//                             return v.toFixed();
+//                         }
+//                     ),
+//                     // НАШ ПОТОК В ПОТОКЕ БУДЕТ ЗАКОНЧЕН. НО ВЫСШИЙ ПОТОК НЕ ЗАВЕРШИТСЯ
+//                     // если вылетела ошибка - СОЗДАЙ ПОТОК с 0 и сразу его "зарезолвь"
+//                     catchError(() => {
+//                         return of(0);
+//                     })
+//                 );
+//         })
+
+        // map((value: number) => {
+        //
+        //     // НАШ ПОТОК НЕ ЗАВЕРШАЕТСЯ
+        //     // try {
+        //     //     return value.toFixed();
+        //     // } catch (err) {
+        //     //     // console.log(err);
+        //     //     // просто верни текущее значение
+        //     //     // return value;
+        //     //
+        //     //     // В "ячейке" вместо значения (value) вернется Observable (т.е. застрартует новый поток)
+        //     //     // тогда наш поток становится ПОТОКОМ ВЫСШЕГО ПОРЯДКА (high order observable)
+        //     //     // если это так, то в subscribe необходимо подписаться И НА ЭТО значение. см (*) чтобы это не делать
+        //     //     return of(0);
+        //     // }
+        //     return value.toFixed();
+        // }),
+        //
+        // // catchError ПРИНИМАЕТ ОШИБКИ. ЗАВЕРШАЕТ НАШ ПОТОК. ВОЗВРАЩАЕТ НОВЫЙ ПОТОК.
+        // catchError((_err: Error, _inner: Observable<string | number>) => {
+        //     // если вылетела ошибка - СОЗДАЙ ПОТОК с 0.
+        //     return of(0);
+        //
+        //     // ВЕРНИ НОВЫЙ ПОТОК в котором повторяются правильные варианты высшего потока снова и снова
+        //     // (его можно так-же прогнать через pipe)
+        //     // return inner;
+        // })
+    // )
+    //
+    // .subscribe((value: number | string | Observable<string | number>) => {
+    //         console.log(value);
+    //     }, (err: Error) => {
+    //         console.error(err);
+    //     },
+    //     () => {
+    //         console.log('complete');
+    //     });
+
+
+// ------------------------
+// еще раз про потоки высшего уровня
+
+// const sequence1$: Observable<number> = interval(1000)
+//     .pipe(take(4));
+// const highOrderSequence$: Observable < Observable<number>> = sequence1$.pipe(
+//     // каждому зачению от sequence1$ присваиваем новый конечный поток со значениями 1, 2
+//     map((_value: number) => of(1, 2))
+// );
+// highOrderSequence$.subscribe((value: Observable<number>) => {
+//         value.subscribe((v: number) => {
+//             console.log(v);
 //         });
+// });
 
-// --------------------
+// но это плохая практика. чтобы не делать subscribe в subscribe делают switchAll. switchMap = map + switchAll
 
-/* TAKE, SKIP */
-// оператор take завершает бесконечный поток
-
-// const sequence8$: Observable<number> = interval(1000);
-
-/*
-* sequence8$ --0--1--2--3--4--5--6--7--8--9--
-*   skip(5) пропусти 5 значение
-*   take(3) возьми 3 значения. и завершает поток
-* */
-
-// const sequence9$: Observable<number> = sequence8$
-//     .pipe(
-//         tap((x: number) => console.log('x--->', x) ), // чтобы показать, что поток идет
-//         skip(5),
-//         take(3)
-//     );
-//
-// sequence9$.subscribe((value: number) => {
-//         console.log('subscribe 1 ----> ', value);
-//     }, () => {},
-//     () => {
-//         console.log('complete');
-//     });
-
-
-// ---------------
-/* CONCAT
-MERGE -> || */
-
-// const sequence10$: Observable<number> = interval(500)
+// const sequence1$: Observable<number> = interval(1000)
 //     .pipe(take(4));
 //
-// const sequence11$: Observable<number> = interval(300)
-//     .pipe(
-//         take(5)
-//     );
-/*
-* sequence10$   ----0----1----2----3|
-* * sequence11$ --0--1--2--3--4|
-*       merge
-* sequence12$ --0-01--21-3--(24)----3| () - одновременно
-* */
-
-// const sequence12$: Observable<number> = merge(sequence10$, sequence11$);
+// const highOrderSequence$: Observable<number> = sequence1$.pipe(
+//     // каждому зачению от sequence1$ присваиваем новый конечный поток со значениями 1, 2
+//     map((_value: number) => of(1, 2)),
+//     switchAll()
+// );
 //
-// sequence12$.subscribe((value: number) => {
-//         console.log('subscribe 1 ----> ', value);
-//     }, () => {},
-//     () => {
-//         console.log('complete');
-//     });
-//
-
-
-// ---------------
-/* combineLatest || */
-
-// const sequence1$: Observable<number> = interval(500)
-//     .pipe(take(4));
-//
-// const sequence2$: Observable<number> = interval(300)
-//     .pipe(
-//         take(5)
-//     );
-
-/*
-* sequence10$   ----0----1----2----3|
-* * sequence11$ --0--1--2--3--4|
-*       combineLatest
-* sequence12$ ----[0,0],[0,1]--[0,2],[1,2]-[1,3]--[2,3][2,4]--[3,4]|
-* */
-
-// const sequence3$: Observable<number[]> = combineLatest(sequence1$, sequence2$);
-//
-// sequence3$.subscribe((value: number[]) => {
-//         console.log('subscribe 1 ----> ', value);
-//     }, () => {},
-//     () => {
-//         console.log('complete');
-//     });
-
+// highOrderSequence$.subscribe((value: number) => {
+//     console.log(value);
+// });
 
 // ----------------------
-/* ZIP */
+// НАПИСАТЬ КАСТОМНЫЙ ОПЕРАТОР: skip(5), take(3), skip(5). тк подобного еще не существует
 
-// const sequence1$: Observable<string> = of('h','e','l','l','o');
-// const sequence2$: Observable<number> = interval(400)
-//     .pipe(
-//         take(5)
-//     );
-
-/*
-* sequence1$   ---(hello)|
-* * sequence2$ ---0---1---2---3---4|
-*       zip((x,y) => x)
-* sequence3$ ---h---e---l---l---o
-* */
-
-// const sequence3$: Observable<string> = zip(sequence1$, sequence2$)
-//     .pipe(map(([x, _y]: [string, number]) => x));
+// создается новый поток по нашей логике
+// class SkipLimitSubscribe extends Subscriber<number> {
 //
-// sequence3$.subscribe((value: string) => {
-//         console.log('subscribe 1 ----> ', value);
-//     }, () => {},
-//     () => {
-//         console.log('complete');
+//     private _interval: number = 1;
+//     private _count: number = 1;
+//
+//     public constructor(subscriber: Subscriber<number>, private _skip: number, private _limit: number) {
+//         super(subscriber);
+//     }
+//
+//     protected _next(value: number): void {
+//         const borderLow: number = this._interval * (this._skip + this._limit) - this._limit;
+//         const borderHight: number = borderLow + this._limit;
+//         if (borderLow < this._count && this._count <= borderHight) {
+//             this.destination && this.destination.next(value);
+//             this._count++;
+//             if (borderHight < this._count) {
+//                 this._interval++;
+//             }
+//             return;
+//         }
+//         this._count++;
+//     }
+// }
+//
+// function skipLimit(skip: number, limit: number): (source: Observable<number>) => Observable<number> {
+//     return (source: Observable<number>): Observable<number> => {
+//         return source.lift({
+//             call(subscriber: Subscriber<number>): void {
+//                 source.subscribe(new SkipLimitSubscribe(subscriber, skip, limit));
+//             }
+//         });
+//     };
+// }
+//
+//
+// interval(1000)
+//     .pipe(skipLimit(4, 3))
+//     .subscribe((value: number) => {
+//         console.log(value);
 //     });
 
 
-// Пример с TouchEvent
-// ---------------------------
-
-// const touchStart$: Observable<number> = ( fromEvent(
-//     document,
-//     'touchstart'
-// ) as Observable<TouchEvent>)
-//     .pipe(map(({ changedTouches }: TouchEvent) => changedTouches[0].clientX));
-//
-// const touchEnd$: Observable<number> = ( fromEvent(
-//     document,
-//     'touchend'
-// ) as Observable<TouchEvent>)
-//     .pipe(map(({ changedTouches }: TouchEvent) => changedTouches[0].clientX));
-//
-//
-// const swipe$: Observable<[number, number]> = zip(touchStart$, touchEnd$);
-//
-// swipe$.subscribe(([startX, endX]: [number, number]) => {
-//         console.log('subscribe 1 ----> ', startX - endX);
-//         if (startX - endX > 0) {
-//             console.log('left swipe');
-//             return;
-//         }
-//         console.log('right swipe');
-//     }, () => {},
-//     () => {
-//         console.log('complete');
-//     });
 
 
-// ------------------
-// Обработка ошибок
+// ------------------------------
+// Контролируемые потоки
+
+
+// iterator + observer = Observable
+// iterator - чтобы во времени распределять какие-то значения.
+// observer - чтобы иметь возможность подписаться и передать значение
+
+// Observable + Observer = Subject
+// Subject по умолчанию - бесконечный контролируемый горячий поток
+// $$ - контролируемый поток
+// Observer - это как Subscriber
+
+// let sequence$$: Subject<string> = new Subject;
 //
-// const sequence1$: Observable<number> = Observable.create((subscriber: Subscriber<number>) => {
-//     let count = 0;
-//     setInterval(() => {
-//         count++;
-//         if (count % 4 === 0) {
-//             subscriber.error(count);
-//             return;
-//         }
-//         subscriber.next(count);
-//     }, 1000);
-//
+// // пока нет подписчика - ничего не увидим. Новое значение с next передаем после подписки
+// // const sub1$: Subscription = sequence$$.subscribe((value: string) => { // чтобы была возможность от него отписатся
+// sequence$$.subscribe((value: string) => {
+//     console.log('sub 1 ----> ', value);
 // });
 //
-// sequence1$.pipe(
-//         catchError( (_err: Error, _out: Observable<number>) => {
-//             return of(1);
-//         })
-//     )
+// sequence$$.next('Hi');
 //
-// .subscribe((value: number) => {
-//         console.log('subscribe 1 ----> ', value);
-//     }, () => {},
-//     () => {
-//         console.log('complete');
+// setTimeout(() => {
+//     console.log('after 5 sec');
+//     sequence$$.subscribe((value: string) => {
+//         console.log('sub 2 ----> ', value);
 //     });
+// }, 5000);
+//
+// setTimeout(() => {
+//     console.log('after 10 sec');
+//     sequence$$.next('Angular');
+// }, 10000);
+
+
+// -------------------------
+// BehaviorSubject
+//
+// const sequence$$: BehaviorSubject<string> = new BehaviorSubject('Initial value');
+// // кеширование последнего значения в потоке. сразу после подписки мы получим значение, переданное при инициализации
+//
+// sequence$$.subscribe((value: string) => {
+//     console.log('sub 0 ----> ', value);
+// });
+//
+// sequence$$.next('Java');
+//
+// sequence$$.subscribe((value: string) => {
+//     console.log('sub 1 ----> ', value);
+// });
+//
+// sequence$$.next('Hi');
+//
+//
+// setTimeout(() => {
+//     console.log('after 5 sec');
+//     sequence$$.subscribe((value: string) => {
+//         console.log('sub 2 ----> ', value);
+//     });
+// }, 5000);
+//
+// setTimeout(() => {
+//     console.log('after 10 sec');
+//     sequence$$.next('Angular');
+// }, 10000);
+
+
+// --------------------
+// охладить поток - ReplaySubject
+// если не передали аргументов - то поток становится полностью холодным, и значит значения забираются с начала
+// если есть арг. по окну x знач-й, то берутся последние x-значений (те поток с x последними знач-ми становится холодным)
+// так же как и поток с x - ms. становится холодным. все что было "emit" x - ms назад попадет в результат
+
+// const sequence$$: ReplaySubject<string> = new ReplaySubject();
+//
+// sequence$$.next('Java');
+//
+// sequence$$.next('Hi');
+//
+// setTimeout(() => {
+//     console.log('after 5 sec');
+//     sequence$$.next('TypeScript');
+//     sequence$$.subscribe((value: string) => {
+//         console.log('sub 2 ----> ', value);
+//     });
+// }, 5000);
+//
+// setTimeout(() => {
+//     console.log('after 10 sec');
+//     sequence$$.next('Angular');
+//     sequence$$.subscribe((value: string) => {
+//         console.log('sub 3 ----> ', value);
+//     });
+// }, 10000);
+
+//--------------------------------
+// AsyncSubject
+
+// const sequence$$: AsyncSubject<string> = new AsyncSubject();
+//
+// sequence$$.subscribe((value: string) => {
+//     console.log('sub 0 ----> ', value);
+// });
+// sequence$$.next('Java');
+//
+// sequence$$.next('Hi');
+//
+// sequence$$.complete();
+//
+// sequence$$.subscribe((value: string) => {
+//     console.log('sub 1 ----> ', value);
+// });
+//
+// setTimeout(() => {
+//     console.log('after 10 sec');
+//     sequence$$.subscribe((value: string) => {
+//         console.log('sub 2 ----> ', value);
+//     });
+// }, 5000);
+
+
+// как сделать холодный поток горячим? -> мультикастинг
+// muliticast + subject = publish, можно использовать его
+// const controlSequence$$: Subject<number> = new Subject;
+
+// const connectableObservable$: ConnectableObservable<number> = interval(1000)
+//     .pipe(
+//         // multicast(controlSequence$$)
+//         publish()
+//     ) as ConnectableObservable<number>;
+//
+// connectableObservable$.subscribe((value: number) => {
+//     console.log('sub 1 -->', value);
+// });
+//
+// // данный Observable ничего не будет делать, пока на него не вызовут .connect()
+// setTimeout(() => {
+//     connectableObservable$.connect();
+// }, 3000);
+//
+//
+// setTimeout(() => {
+//     // и тут наш поток становится горячим!!!
+//     connectableObservable$.subscribe((value: number) => {
+//         console.log('sub 2 -->', value);
+//     });
+// }, 6000);
+
+
+// РАСПИСАНИЕ
+
+console.log('Start');
+setTimeout(() => console.log('timeout 1'));
+setTimeout(() => console.log('timeout 2'));
+Promise.resolve()
+    .then(() => {console.log('promise 1'); });
+Promise.resolve()
+    .then(() => {console.log('promise 2'); });
+
+
+const arr: number[] = [];
+for (let i: number = 0; i < 10000; i++) {
+    arr.push(i);
+}
+
+console.time('Schedule');
+
+// все выполняется синхронно
+from(arr)
+// тут observeOn(async) говорит, что все что происходит в map надо рассматривать как макротаск(Ajax-запрос)
+    // чтобы с map работать в концепции микротаска - надо использовать observeOn(async)
+    // это значит что каждый map уйдет на новый виток eventLoop
+    .pipe(observeOn(asap), map((y: number) => y * 2 % 3))
+    .subscribe(() => {}, () => {},() => {
+        console.timeEnd('Schedule');
+    });
+
+console.log('End');
